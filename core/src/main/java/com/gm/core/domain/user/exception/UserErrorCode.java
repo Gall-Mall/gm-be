e4ba4e7d90b2564cd675313d02a5ab88bd6b6fca
Minhyeok Choi <bbbim5656@gmail.com>
2026-07-24 14:13:40 +0900

    feat : 에러코드 추가

--- core/src/main/java/com/gm/core/domain/user/exception/UserErrorCode.java
+++ core/src/main/java/com/gm/core/domain/user/exception/UserErrorCode.java
@@ -16,7 +16,9 @@ public enum UserErrorCode implements ErrorCode {
     INVALID_ONBOARDING_STATUS(409,"USER-002","현재 온보딩 상태에서는 요청을 처리할 수 없습니다."),
     CATEGORY_PREFERENCE_CONFLICT(400, "USER-003", "선호 카테고리와 비선호 카테고리가 중복되었습니다."),
     MENU_PREFERENCE_CONFLICT(400, "USER-004", "선호 메뉴와 비선호 메뉴가 중복되었습니다."),
-    CATEGORY_NOT_FOUND(404, "USER-005", "카테고리를 찾을 수 없습니다.");
+    CATEGORY_NOT_FOUND(404, "USER-005", "카테고리를 찾을 수 없습니다."),
+    INVALID_USER_INPUT(400, "USER-006", "유효하지 않은 입력입니다"),
+    TERMS_AGREED_APPROVE(400, "USER-007", "필수 약관에 동의해야 합니다.");
 
 
     private final int status;
